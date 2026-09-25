7b3332e9515f4c42f3e6c8d14229635b1c8d0ac6
Christie Thomas <53096466+clynnt@users.noreply.github.com>
2026-09-25 16:05:50 -0500

    Add files via upload

--- reporting_app/ts/TS_Acq_DuplicateOrders.sql
+++ reporting_app/ts/TS_Acq_DuplicateOrders.sql
@@ -1,10 +1,10 @@
 --metadb:function TS_Acq_DuplicateOrders
 
-DROP FUNCTION IF EXISTS TS_Acq_DuplicateOrders(date, date);
+DROP FUNCTION IF EXISTS TS_Acq_DuplicateOrders;
 
 CREATE FUNCTION TS_Acq_DuplicateOrders(
-    start_date date DEFAULT CURRENT_DATE - 1,
-    end_date date DEFAULT CURRENT_DATE
+    start_date date, --DEFAULT CURRENT_DATE - 1,
+    end_date date --DEFAULT CURRENT_DATE
 )
 RETURNS TABLE ( -- dup orders results mapping
     po_number text,
@@ -37,52 +37,48 @@ AS
 $$
 WITH
 new_orders as (
-    WITH 
+    WITH
     orders as (
         SELECT
             po.po_number AS po_number,
-            po.date_ordered::date as date_ordered,
-            po.workflow_status AS workflow_status,
-            org.code as vendor_code,
+            po.created_date::date as date_ordered,
+            po.po_workflow_status AS workflow_status,
+            po.vendor_code as vendor_code,
             CASE 
-                WHEN jsonb_extract_path_text(pol.jsonb, 'cost', 'listUnitPrice') IS NOT NULL THEN jsonb_extract_path_text(pol.jsonb, 'cost', 'listUnitPrice')
-                ELSE jsonb_extract_path_text(pol.jsonb, 'cost', 'listUnitPriceElectronic')
+                WHEN pol_c.po_line_list_unit_price_phys IS NOT NULL THEN pol_c.po_line_list_unit_price_phys
+                ELSE pol_c.po_line_list_unit_price_elec
             END AS price,
-            plt.order_format AS order_format,
-            plt.rush AS rush,
-            plt.title_or_package AS po_instance_title,
-            plt.publication_date AS po_publication_date,
-            plt.publisher AS po_publisher,
-            plt.instance_id as instance_id,
-            plt.id as pol_id
-        FROM folio_orders.purchase_order__t po
-        LEFT JOIN folio_orders.po_line as pol on pol.purchaseorderid = po.id
-        LEFT JOIN folio_orders.po_line__t as plt on plt.po_line_number = jsonb_extract_path_text(pol.jsonb, 'poLineNumber')
-        LEFT JOIN folio_organizations.organizations__t org on po.vendor = org.id
+            plt.order_format AS order_format, -- can't find in derived tables
+            po.rush AS rush,
+            po.title AS po_instance_title,
+            po.publication_date AS po_publication_date,
+            po.publisher AS po_publisher,
+            po.pol_instance_id as instance_id,
+            po.po_line_id as pol_id
+        FROM folio_derived.po_instance po
+        LEFT JOIN folio_derived.po_lines_cost pol_c ON pol_c.pol_id = po.po_line_id
+        LEFT JOIN folio_orders.po_line__t plt ON plt.id = po.po_line_id
         WHERE 
-            po.date_ordered::date >= start_date
-            AND po.date_ordered::date < end_date
+			po.created_date::date >= start_date
+			AND created_date::date < end_date
         ),
     new_identifiers as (
         SELECT
-            ino.id as instance_id,
-            it.hrid AS instance_hrid,
-            itt.name as identifier_type_name,
-            identifiers.jsonb #>> '{value}' as raw_identifier,
+            ni.instance_id as instance_id,
+            ni.instance_hrid AS instance_hrid,
+            ni.identifier_type_name as identifier_type_name,
+            ni.identifier as raw_identifier,
             CASE
-            WHEN itt.name IN ('ISBN', 'Invalid ISBN') THEN
-                NULLIF(regexp_replace(upper(trim(identifiers.jsonb #>> '{value}')), '( .*)|[^0-9X]', '', 'g'), '')
+            WHEN ni.identifier_type_name IN ('ISBN', 'Invalid ISBN') THEN
+                NULLIF(regexp_replace(upper(trim(ni.identifier)), '( .*)|[^0-9X]', '', 'g'), '')
             ELSE
-                regexp_replace(trim(identifiers.jsonb #>> '{value}'), '^\(OCoLC\)(ocm|ocn|om|on)?', '')
+                regexp_replace(trim(ni.identifier), '^\(OCoLC\)(ocm|ocn|om|on)?', '')
             END AS identifier
         FROM orders oi
-        LEFT JOIN folio_inventory.instance ino ON ino.id = oi.instance_id
-            CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(ino.jsonb, 'identifiers')) as identifiers (jsonb)
-        LEFT JOIN folio_inventory.identifier_type__t itt on (identifiers.jsonb #>> '{identifierTypeId}')::uuid = itt.id
-        LEFT JOIN folio_inventory.instance__t it ON it.id = ino.id
-        WHERE itt.name IN ('ISBN', 'Invalid ISBN', 'OCLC', 'Cancelled system control number') 
+        LEFT JOIN folio_derived.instance_identifiers ni ON ni.instance_id = oi.instance_id
+        WHERE ni.identifier_type_name IN ('ISBN', 'Invalid ISBN', 'OCLC', 'Cancelled system control number')
         ),
-    donor_codes AS ( 
+    donor_codes AS ( -- no derived table version?
         SELECT 
             string_agg(ot.code, ' | ') as donor_codes,
             pol2.id as pol_id
@@ -94,15 +90,14 @@ new_orders as (
         ),
     fund_codes AS (
         SELECT
-            string_agg((fundDis.jsonb #>> '{code}'), ' | ') AS funds,
-            pol3.id AS pol_id
-        FROM folio_orders.po_line pol3 
-            CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'fundDistribution')) AS fundDis (jsonb)
-        GROUP BY pol3.id
+            string_agg(po_f.fund_code, ' | ') AS funds,
+            po_f.po_line_id AS pol_id
+        FROM folio_derived.po_lines_fund_distribution_transactions po_f
+        GROUP BY po_f.po_line_id
     )
     SELECT
         o.po_number AS po_number,
-        it.hrid as po_instance_hrid,
+        it.instance_hrid as po_instance_hrid,
         o.vendor_code as vendor_code,
         o.date_ordered::date as date_ordered,
         o.workflow_status AS workflow_status,
@@ -117,31 +112,28 @@ new_orders as (
         noi.raw_identifier AS raw_identifier,
         noi.identifier AS order_identifier
     FROM orders o
-    LEFT JOIN folio_inventory.instance__t it on it.id = o.instance_id
+    LEFT JOIN folio_derived.instance_ext it on it.instance_id = o.instance_id
     LEFT JOIN donor_codes as dc on dc.pol_id = o.pol_id
     LEFT JOIN fund_codes as fc on fc.pol_id = o.pol_id
     LEFT JOIN new_identifiers noi on noi.instance_id = o.instance_id
     ),
 identifiers AS ( -- all identifiers in FOLIO
-    SELECT
-        i.id as instance_id,
-        it.hrid AS instance_hrid,
-        itt.name as identifier_type_name,
-        identifiers.jsonb #>> '{value}' as raw_identifier,
-    CASE
-        WHEN itt.name IN ('ISBN', 'Invalid ISBN') THEN
-            NULLIF(regexp_replace(upper(trim(identifiers.jsonb #>> '{value}')), '( .*)|[^0-9X]', '', 'g'), '')
-        ELSE
-            regexp_replace(trim(identifiers.jsonb #>> '{value}'), '^\(OCoLC\)(ocm|ocn|om|on)?', '')
-        END AS identifier
-    FROM folio_inventory.instance i
-        CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'identifiers')) as identifiers (jsonb)
-    LEFT JOIN folio_inventory.identifier_type__t itt on (identifiers.jsonb #>> '{identifierTypeId}')::uuid = itt.id
-        LEFT JOIN folio_inventory.instance__t it ON it.id = i.id
-    WHERE itt.name IN ('ISBN', 'Invalid ISBN', 'OCLC', 'Cancelled system control number') 
+	SELECT
+		ii.instance_id as instance_id,
+		ii.instance_hrid AS instance_hrid,
+		ii.identifier_type_name as identifier_type_name,
+		ii.identifier as raw_identifier,
+		CASE
+		WHEN ii.identifier_type_name IN ('ISBN', 'Invalid ISBN') THEN
+			NULLIF(regexp_replace(upper(trim(ii.identifier)), '( .*)|[^0-9X]', '', 'g'), '')
+		ELSE
+			regexp_replace(trim(ii.identifier), '^\(OCoLC\)(ocm|ocn|om|on)?', '')
+		END AS identifier
+	FROM folio_derived.instance_identifiers ii
+	WHERE ii.identifier_type_name IN ('ISBN', 'Invalid ISBN', 'OCLC', 'Cancelled system control number')
     ),
 folio_matches AS (
-	SELECT DISTINCT ON (matched_ids.instance_id)
+	SELECT
 		ord.po_number AS new_po_number,
 		matched_ids.instance_id AS instance_id
 	FROM new_orders ord 
@@ -149,49 +141,42 @@ folio_matches AS (
 ),   
 contributors_agg AS ( --Step #5 Check for instances that match on the ISBN or the OCN in the instances attached to the PO.
 	SELECT
-		ic.id AS instance_id,
-		string_agg((contributors.jsonb #>> '{name}'), ' | ') AS contributors
+		ic.instance_id AS instance_id,
+		string_agg(ic.contributor_name, ' | ') AS contributors
 	FROM folio_matches fmc
-	LEFT JOIN folio_inventory.INSTANCE ic ON ic.id = fmc.instance_id
-		CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(ic.jsonb, 'contributors')) AS contributors (jsonb)
-	GROUP BY ic.id
+	LEFT JOIN folio_derived.instance_contributors ic ON ic.instance_id = fmc.instance_id
+	GROUP BY ic.instance_id
     ),
 pub_dates_agg AS (
 	SELECT
-		ip.id AS instance_id,
-		string_agg((pub_info.jsonb #>> '{dateOfPublication}'), ' | ') AS dates
+		ip.instance_id AS instance_id,
+		string_agg(ip.date_of_publication, ' | ') AS dates
 	FROM folio_matches fmp
-	LEFT JOIN folio_inventory.INSTANCE ip ON ip.id = fmp.instance_id
-		CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(ip.jsonb, 'publication')) AS pub_info (jsonb)
-	GROUP BY ip.id
+	LEFT JOIN folio_derived.instance_publication ip ON ip.instance_id = fmp.instance_id
+	GROUP BY ip.instance_id
     ),
 editions_agg AS (
 	SELECT
-		ie.id AS instance_id,
-		string_agg((ed.jsonb #>> '{}'), ' | ') AS editions
+		ie.instance_id AS instance_id,
+		string_agg(ie.edition, ' | ') AS editions
 	FROM folio_matches fme
-	LEFT JOIN folio_inventory.INSTANCE ie ON ie.id = fme.instance_id
-		CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(ie.jsonb, 'editions')) AS ed (jsonb)
-	GROUP BY ie.id
+	LEFT JOIN folio_derived.instance_editions ie ON ie.instance_id = fme.instance_id
+	GROUP BY ie.instance_id
     ),
 stat_code_agg AS (
 	SELECT
-		isc.id AS instance_id,
-		string_agg(isct.code, ' | ') AS stat_codes
+		isc.instance_id AS instance_id,
+		string_agg(isc.statistical_code, ' | ') AS stat_codes
 	FROM folio_matches fmsc
-	LEFT JOIN folio_inventory.INSTANCE isc ON isc.id = fmsc.instance_id
-    	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(isc.jsonb, 'statisticalCodeIds')) AS stat_codes (jsonb)
-	LEFT JOIN folio_inventory.statistical_code__t isct ON isct.id = (stat_codes.jsonb #>> '{}')::uuid
-	GROUP BY isc.id
+	LEFT JOIN folio_derived.instance_statistical_codes isc ON isc.instance_id = fmsc.instance_id
+	GROUP BY isc.instance_id
 ),
 holdings_agg AS (
     SELECT
         hrt.instance_id AS instance_id,
-        string_agg(htt.name, ',') AS holdings_types,
-        string_agg(DISTINCT lt.name, ' | ') AS holdings_locs
-    FROM folio_inventory.holdings_record__t hrt
-    LEFT JOIN folio_inventory.holdings_type__t htt ON hrt.holdings_type_id = htt.id
-    LEFT JOIN folio_inventory.location__t lt ON hrt.permanent_location_id = lt.id 
+        string_agg(hrt.type_name, ' | ') AS holdings_types,
+        string_agg(DISTINCT hrt.permanent_location_name, ' | ') AS holdings_locs
+    FROM folio_derived.holdings_ext hrt
     GROUP BY hrt.instance_id
     )
 SELECT DISTINCT ON (ord.po_instance_hrid, fm.instance_id)
@@ -210,10 +195,10 @@ SELECT DISTINCT ON (ord.po_instance_hrid, fm.instance_id)
     ord.identifier_type_name AS po_identifier_type,
     ord.raw_identifier AS po_raw_identifier,
     ord.order_identifier AS po_identifier,
-    iton.hrid as matched_instance_hrid,
-    iton.title as matched_title,
-    iton.discovery_suppress AS matched_suppressed,
-    ist.name as matched_status,
+    itm.instance_hrid as matched_instance_hrid,
+    itm.title as matched_title,
+    itm.discovery_suppress AS matched_suppressed,
+    itm.status_name as matched_status,
     sca.stat_codes AS matched_stat_codes,
     cont.contributors as matched_contributors,
     pd.dates as matched_pub_dates,
@@ -222,8 +207,7 @@ SELECT DISTINCT ON (ord.po_instance_hrid, fm.instance_id)
     ha.holdings_locs as matched_holdings_locations
 FROM new_orders ord 
 JOIN folio_matches fm ON fm.new_po_number = ord.po_number
-LEFT JOIN folio_inventory.instance__t iton ON iton.id = fm.instance_id
-LEFT JOIN folio_inventory.instance_status__t ist ON ist.id = iton.status_id
+LEFT JOIN folio_derived.instance_ext itm ON itm.instance_id = fm.instance_id
 LEFT JOIN contributors_agg AS cont ON fm.instance_id = cont.instance_id
 LEFT JOIN pub_dates_agg AS pd ON fm.instance_id = pd.instance_id
 LEFT JOIN editions_agg AS ed ON fm.instance_id = ed.instance_id
